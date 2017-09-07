4496080053c77260179ec0ff9d77ac69c2407e75
wangjiechen <wang_jiechen@163.com>
2017-09-08 00:22:25 +0800

    day 1_2

--- alien_invasion/alian_invasion.py
+++ alien_invasion/alian_invasion.py
@@ -7,3 +7,20 @@ def run_game():
     pygame.init()                                       
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alian_Invasion")
+    
+    
+    #开始游戏的主循环
+    while True:
+        
+        
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+                
+                
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
