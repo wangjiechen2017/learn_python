855a0b40f2cd090aa3dad82bc239a3a49468522e
wangjiechen <wang_jiechen@163.com>
2017-09-08 00:01:28 +0800

    alian_invasion day 1

---
+++ alien_invasion/alian_invasion.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import sys
+import pygame
+
+def run_game():    
+    #初始化游戏并创建一个屏幕对象           
+    pygame.init()                                       
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alian_Invasion")
