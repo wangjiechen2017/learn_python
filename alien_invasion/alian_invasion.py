93c8bd47615168a9a646207dbca83e59ce9a4d59
wangjiechen <wang_jiechen@163.com>
2017-09-10 09:54:11 +0800

    day-------4 add game_functions.py

--- alien_invasion/alian_invasion.py
+++ alien_invasion/alian_invasion.py
@@ -1,12 +1,16 @@
 # -*- coding:utf-8 -*-
-import sys
+#import sys
 import pygame
+
 from settings import Settings
 from ship import Ship
+import game_functions as gf
+
+
 
 def run_game():    
     #初始化游戏并创建一个屏幕对象           
-    pygame.init()            
+    gf.pygame.init()            
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width,ai_settings.screen_height))
@@ -18,15 +22,19 @@ def run_game():
     #开始游戏的主循环
     while True:
             
-        #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        #~ #监视键盘和鼠标事件
+        #~ for event in pygame.event.get():
+            #~ if event.type == pygame.QUIT:
+                #~ sys.exit()
+        
+        gf.check_events()
                 
-        #每次循环时重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        #~ #每次循环时重绘屏幕
+        #~ screen.fill(ai_settings.bg_color)
+        #~ ship.blitme()
+        #~ #让最近绘制的屏幕可见
+        #~ pygame.display.flip()
+        
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
