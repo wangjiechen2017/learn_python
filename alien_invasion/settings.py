684d409600f6e24e584472fc6cbbf433d0afa8d7
wangjiechen <wang_jiechen@163.com>
2017-09-26 23:01:18 +0800

    Page244 ready to create a new branch P244

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,7 +8,7 @@ class Settings():
         #屏幕设置
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230,230)q
+        self.bg_color = (230,230,230)
         #飞船的设置
         self.ship_speed_factor = 1.5
 
